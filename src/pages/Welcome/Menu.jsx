023151d54ea88e5e46af12baa05ec1d919a6df47
vanStaen <clement.vanstaen@gmail.com>
2023-06-25 17:37:04 +0200

    federlogo in menu

--- src/pages/Welcome/Menu.jsx
+++ src/pages/Welcome/Menu.jsx
@@ -1,36 +1,44 @@
 import React, { useState } from "react";
 
-import telegramLogoMenu from "../../img/telegramLogoMenu.png";
+import federLogo from "../../img/federLogo.png";
 
 import "./Menu.less";
 
 export const Menu = () => {
-    const [showMenu, setShowMenu] = useState(true);
+  const [showMenu, setShowMenu] = useState(true);
 
-    return (
-        <>
-            <div className="menu">
-                <div className={showMenu ? 'menu-bar' : 'menu-bar change'} onClick={() => setShowMenu(!showMenu)}>
-                    <div id="bar1" className="bar"></div>
-                    {showMenu && <div className="spacer"></div>}
-                    <div id="bar2" className="bar"></div>
-                    {showMenu && <div className="spacer"></div>}
-                    <div id="bar3" className="bar"></div>
-                </div>
-                <nav className={showMenu ? 'nav' : 'nav change'}>
-                    <ul className="list">
-                        <li><a href="#">Presskit</a></li>
-                        <li><a href="#">Booking</a></li>
-                        <li><a href="#">About</a></li>
-                        <li>
-                            <a href='https://t.me/SCHWERELOS_BERLIN' target='_blank'>
-                                <img src={telegramLogoMenu} className="logoTelegramMenu" />
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-            </div >
-            <div className={showMenu ? 'menu-bg' : 'menu-bg change-bg'} id="menu-bg"></div>
-        </>
-    );
-}; 
+  return (
+    <>
+      <div className="menu">
+        <div
+          className={showMenu ? "menu-bar" : "menu-bar change"}
+          onClick={() => setShowMenu(!showMenu)}
+        >
+          <div id="bar1" className="bar"></div>
+          {showMenu && <div className="spacer"></div>}
+          <div id="bar2" className="bar"></div>
+          {showMenu && <div className="spacer"></div>}
+          <div id="bar3" className="bar"></div>
+        </div>
+        <nav className={showMenu ? "nav" : "nav change"}>
+          <ul className="list">
+            <li>
+              <a href="#">Presskit</a>
+            </li>
+            <li>
+              <a href="#">Booking</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <img src={federLogo} className="federLogo" />
+          </ul>
+        </nav>
+      </div>
+      <div
+        className={showMenu ? "menu-bg" : "menu-bg change-bg"}
+        id="menu-bg"
+      ></div>
+    </>
+  );
+};
