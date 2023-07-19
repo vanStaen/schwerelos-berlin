1ca6353b789774ec99d801523ecd100714c6cb3c
vanStaen <clement.vanstaen@gmail.com>
2023-07-19 14:00:45 +0200

    showDev

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -6,36 +6,38 @@ import { Artist } from "./Artist/Artist";
 import { Events } from "./Events/Events";
 import { Merch } from "./Merch/Merch";
 import { Tickets } from "./Tickets/Tickets";
+import { showDev } from "../helpers/showDev";
 
 export const Main = () => {
   const isProduction = process.env.NODE_ENV === "production";
 
-  const pages = isProduction
-    ? [
-        <div key="welcomePage">
-          <Welcome />
-        </div>,
-        <div key="eventPage">
-          <Events />
-        </div>,
-      ]
-    : [
-        <div key="welcomePage">
-          <Welcome />
-        </div>,
-        <div key="eventPage">
-          <Events />
-        </div>,
-        <div key="artistsPage">
-          <Artist />
-        </div>,
-        <div key="merchPage">
-          <Merch />
-        </div>,
-        <div key="ticketPage">
-          <Tickets />
-        </div>,
-      ];
+  const pages =
+    isProduction && showDev()
+      ? [
+          <div key="welcomePage">
+            <Welcome />
+          </div>,
+          <div key="eventPage">
+            <Events />
+          </div>,
+        ]
+      : [
+          <div key="welcomePage">
+            <Welcome />
+          </div>,
+          <div key="eventPage">
+            <Events />
+          </div>,
+          <div key="artistsPage">
+            <Artist />
+          </div>,
+          <div key="merchPage">
+            <Merch />
+          </div>,
+          <div key="ticketPage">
+            <Tickets />
+          </div>,
+        ];
 
   return (
     <>
