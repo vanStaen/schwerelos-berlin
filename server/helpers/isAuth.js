e0890c16e0e270aab303f1834668659ecce893d4
vanStaen <clement.vanstaen@gmail.com>
2023-07-26 16:09:00 +0200

    in dev mode

--- server/helpers/isAuth.js
+++ server/helpers/isAuth.js
@@ -1,7 +1,7 @@
 const jsonwebtoken = require("jsonwebtoken");
 require("dotenv/config");
 
-const devMode = false;
+const devMode = true;
 
 module.exports = async (req, res, next) => {
     // if in development mode
