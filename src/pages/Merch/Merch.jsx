cd851902b3ed36a49e6486eb2acbc36af74463c0
vanStaen <clement.vanstaen@gmail.com>
2023-08-03 10:33:39 +0200

    merch translation

--- src/pages/Merch/Merch.jsx
+++ src/pages/Merch/Merch.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { observer } from "mobx-react";
+import { useTranslation } from "react-i18next";
+
 import { GlitchText } from "../../components/GlitchText/GlitchText";
 
 import "./Merch.less";
 
 export const Merch = observer(() => {
+  const { t } = useTranslation();
   return (
     <div className="pageMerchContainer">
       <div className="backgroundOpacity"></div>
       <div className="merchContainer">
-        <GlitchText glitchText="Merch" overText="come get some" />
+        <GlitchText glitchText={t('merch.merch')} overText={t('merch.comeGetSome')} />
       </div>
     </div>
   );
